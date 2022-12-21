16747cea01095b72c6f71ffbb135fa9177abd14c
Никита Ульянов <nik.ule@yandex.ru>
2022-12-22 01:55:05 +0300

    add sum

--- src/Matrices.java
+++ src/Matrices.java
@@ -7,8 +7,19 @@ public final class Matrices {
                 aMatrix.getWidth() != bMatrix.getWidth())
             throw new RuntimeException("руки кривые: нельзя скадывать матрицы разных размеров по законам алгебры :с");
 
-//          TODO
-        return null;
+        int height = aMatrix.getHeight();
+        int width = aMatrix.getWidth();
+        int[][] values = new int[height][width];
+        int[][] aVal = aMatrix.getValues();
+        int[][] bVal = bMatrix.getValues();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                values[y][x] = aVal[y][x] + bVal[y][x];
+            }
+        }
+
+        return new Matrix(height, width, values);
     }
 
     public static Matrix multiply(Matrix matrix, int n) {
